60dcfab5a17122a4dcf4317c647c8de18e0190c8
Gonzalo Moncada <gmitiweb@gmail.com>
2024-05-15 18:02:46 -0500

    Ajustes en la barra

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -1,12 +1,11 @@
 import { Navbar, Nav, Container,NavDropdown,Form,Button  } from "react-bootstrap"
 import { Outlet, Link } from "react-router-dom"
 import React, { useContext } from "react";
-// import { Link } from "react-router-dom";
 import { CartContext } from "../context/ShoppingCartContext";
 import { CartIcon } from '../components/icon'
+
 const NavBarExample = () => {
     const [cart, setCart] = useContext(CartContext);
-
     const quantity = cart.reduce((acc, curr) => {
       return acc + curr.quantity;
     }, 0);
@@ -15,8 +14,8 @@ const NavBarExample = () => {
       color: "#fff",
       listStyle: "none",
       textDecoration: "none",
-     
     };
+
     return(
        <>    
        <Navbar className="navBg" variant="dark" expand="lg">
@@ -29,29 +28,23 @@ const NavBarExample = () => {
                 <Nav.Link as={Link} to="/hombre">Hombre</Nav.Link>
                 <Nav.Link as={Link} to="/mujer">Mujer</Nav.Link>     
                 <Nav.Link as={Link} to="/electronica">Electronica</Nav.Link>      
-                
-               
                 <NavDropdown title="Categorias" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/hombre">Hombres</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/mujer">Mujeres</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/electronica">Electronica</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+                <NavDropdown.Item as={Link} to="/hombre">Hombres</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/mujer">Mujeres</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/electronica">Electronica</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
          
                 <Nav.Link  as={Link} to="/login">Login</Nav.Link>             
                 <Nav.Link as={Link} to={"/cart"} style={navStyles}>
-                <li >
-                <span className="cart-count">
-                <CartIcon />
-                {quantity}</span>
-                </li>
+                  <li ><span className="cart-count">
+                    <CartIcon />{quantity}</span>
+                  </li>
                 </Nav.Link>
             
-            
-                
             </Nav>
             </Navbar.Collapse>
         </Container>
