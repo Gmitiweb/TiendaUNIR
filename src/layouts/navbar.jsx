8c47c7e4d15e5906f269af3a3625dbfbfa64dbb6
Gonzalo Moncada <gmitiweb@gmail.com>
2024-05-16 14:31:26 -0500

    menu icono a la derecha

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -4,7 +4,7 @@ import React, { useContext } from "react";
 import { CartContext } from "../context/ShoppingCartContext";
 import { CartIcon } from '../components/icon'
 
-const NavBarExample = () => {
+const NavBarTienda = () => {
     const [cart, setCart] = useContext(CartContext);
     const quantity = cart.reduce((acc, curr) => {
       return acc + curr.quantity;
@@ -29,13 +29,13 @@ const NavBarExample = () => {
                     <Nav.Link as={Link} to="/mujer">Mujer</Nav.Link>     
                     <Nav.Link as={Link} to="/electronica">Electronica</Nav.Link>      
                     <Nav.Link  as={Link} to="/pedidos">Pedidos</Nav.Link>   
-                    <Nav.Link  as={Link} to="/login">Login</Nav.Link>             
-                    <Nav.Link as={Link} to={"/cart"} style={navStyles}>
-                      <li ><span className="cart-count">
-                        <CartIcon />{quantity} </span>
-                      </li>
-                    </Nav.Link>
-                    
+                    <div className="loginNav d-flex">
+                        <Nav.Link  as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link as={Link} to={"/cart"} style={navStyles}>
+                             <li ><span className="cart-count">
+                              <CartIcon />{quantity} </span></li>
+                        </Nav.Link>
+                    </div>
                 </Nav>
               </Navbar.Collapse>
           </Container>
@@ -46,4 +46,4 @@ const NavBarExample = () => {
        </> 
     )
 }
-export default NavBarExample
+export default NavBarTienda
